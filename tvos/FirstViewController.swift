7fd2d73f5842b728ffa9be11dcb21c89b28e6748
keakalamudo <kelo.akalamudo@gmail.com>
2016-10-15 20:18:13 -0700

    Intial Commit

---
+++ tvos/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tvos
+//
+//  Created by Kelo Akalamudo on 10/15/16.
+//  Copyright © 2016 Kaymuda. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
